e96dca3abe4839ec6c1905e3f15a52359ebefddd
Lidormon <155873276+Lidormon@users.noreply.github.com>
2024-02-27 21:20:26 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,5 +1,7 @@
 #include "StrList.h"
 #include <stdio.h>
+#include <string.h> // Include this header for strtok
+
 int main()
 {
     StrList *head = StrList_alloc();
@@ -8,43 +10,24 @@ int main()
     int firstTimeFlag = 1;
     do
     {
-        // printf("\nMenu:\n");
-        // printf("1. Put string at the end\n");
-        // printf("2. Put string at index\n");
-        // printf("3. Print list\n");
-        // printf("4. Print list size\n");
-        // printf("5. Print string at index\n");
-        // printf("6. Print number of characters in the list\n");
-        // printf("7. Print occurrences of a string\n");
-        // printf("8. Delete all occurrences of a string\n");
-        // printf("9. Delete at index\n");
-        // printf("10. Reverse the list\n");
-        // printf("11. Delete entire list\n");
-        // printf("12. Arrange list in lexicographic order\n");
-        // printf("13. Check if the list is in lexicographic order\n");
-        // printf("0. Exit\n");
-
-        // printf("Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice)
         {
         case 1:
-            // printf("number of words: ");
             scanf("%d", &index);
-            // printf("Enter string: ");
-            scanf(" %[^\n]", inputString);          // Reads the whole line including spaces
+            scanf(" %[^\n]", inputString); // Reads the whole line including spaces
             char *token = strtok(inputString, " "); // Tokenize the string by space
 
             for (int i = 0; i < index; i++)
             {
                 StrList_insertLast(head, token);
-                char* str;
-                token = strtok(str, " "); // Move to the next token
+                // Move to the next token
+                token = strtok(NULL, " ");
             }
-            
 
             break;
+        }
 
         case 2:
             // printf("Enter index: ");
